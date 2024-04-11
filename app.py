e688896aad88c37516c1a001cd81ff75391aa127
manu m b <mbmanu2000@gmail.com>
2024-04-11 18:53:28 +0530

    added

--- app.py
+++ app.py
@@ -11,11 +11,14 @@ app = Flask(__name__)
 # app.config['MYSQL_USER'] = 'root'
 # app.config['MYSQL_PASSWORD'] = ''
 # app.config['MYSQL_DB'] = 'crisis'
-app.secret_key = 'your_secret_key'
+app.secret_key = os.environ.get('SECRET_KEY')
 
-# MySQL configuration
-import config
-app.config.from_object('config')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST')
+app.config['MYSQL_PORT'] = int(os.environ.get('MYSQL_PORT'))
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB')
+app.config['MYSQL_SSL_MODE'] = os.environ.get('MYSQL_SSL_MODE', 'REQUIRED')
 
 mysql = MySQL(app)
 
